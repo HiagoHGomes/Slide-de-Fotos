0e1806f5d9ff955d329598e37339a335b214479d
Hiago Henrique <hiago_h3@hotmail.com>
2022-09-22 22:41:54 -0300

    Adição javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+let totalslides = document.querySelectorAll('.slide--item').length;
+let currentSlide = 0;
+document.querySelector('.slide--width').style.width = `calc(100vw*${totalslides})`;
+document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
+
+
+function voltar() {
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalslides -1;
+    }
+    updateMargin(); 
+}
+
+function proximo() {
+    currentSlide++;
+    if(currentSlide>(totalslides-1)) {
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function  updateMargin() {
+    let newMargin = (currentSlide * document.body.clientWidth);
+    document.querySelector('.slide--width').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(proximo, 2500);
